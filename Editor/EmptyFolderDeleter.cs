f120faba2fa0ee502ad5f853b1e0e496f6593fc0
baba_s <shotababa1206@gmail.com>
2019-09-28 19:26:26 +0900

    first commit

---
+++ Editor/EmptyFolderDeleter.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace KoganeUnityLib
+{
+	public static class EmptyFolderDeleter
+	{
+		[MenuItem( "Assets/Delete Empty Folder" )]
+		private static void Delete()
+		{
+			DoDelete( "Assets" );
+
+			AssetDatabase.Refresh();
+		}
+
+		private static void DoDelete( string path )
+		{
+			foreach ( var dir in Directory.GetDirectories( path ) )
+			{
+				DoDelete( dir );
+
+				var files = Directory.GetFiles( dir );
+
+				if ( files.Length != 0 ) continue;
+
+				var dirs = Directory.GetDirectories( dir );
+
+				if ( dirs.Length != 0 ) continue;
+
+				AssetDatabase.DeleteAsset( dir );
+			}
+		}
+	}
+}
